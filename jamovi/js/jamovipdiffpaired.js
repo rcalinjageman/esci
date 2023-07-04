bf92332610e31202426a011a92d491892d76c45a
Damian Dropmann <damiandropmann@gmail.com>
2023-07-05 01:03:36 +1000

    Suggested Improvements
    - Added the mode selector
    - A few ui simplifications

--- jamovi/js/jamovipdiffpaired.js
+++ jamovi/js/jamovipdiffpaired.js
@@ -1,59 +1,14 @@
     module.exports = {
 
-        // event handlers and functions are defined here
-
-        // this is an example of an event handler
-        view_updated: function(ui, event) {
-            this.setPanels(ui, event);
+        view_updated: function (ui, event) {
+            ui.alpha.setValue((1 - Number(ui.conf_level.value()) / 100).toPrecision(1));
         },
 
-        view_creating: function(ui, event) {
-            this.setPanels(ui, event);
-        },
-
-        view_loaded: function(ui, event) {
-            // do something
-            this.setPanels(ui, event);
-        },
-
-
-        raw_button_changed: function(ui, event) {
-            this.setPanels(ui, event);
+        view_loaded: function (ui, event) {
+            ui.alpha.setValue((1 - Number(ui.conf_level.value()) / 100).toPrecision(1));
         },
 
         conf_level_changed: function(ui, event) {
             ui.alpha.setValue((1 - Number(ui.conf_level.value())/100).toPrecision(1));
-        },
-
-
-        case_label_changed: function(ui, event) {
-            ui.case_label_repeat.setValue(ui.case_label.value());
-        },
-
-        not_case_label_changed: function(ui, event) {
-            ui.not_case_label_repeat.setValue(ui.not_case_label.value());
-        },
-
-
-        // this is an example of an auxiliary function
-        setPanels: function(ui, event) {
-            // check if summary or raw data option selected, and then set panel and enabled states appropriately
-            if (ui.summary_button.value()){
-              // summary data option - expand that panel and collapse the raw panel
-              // also set all controls in summary panel to be enabled
-              // would be nice to be able to disable the controls in the raw data panel and/or expand option for that panel
-              ui.spanel.expand();
-              ui.rpanel.collapse();
-            } else {
-              // raw data selected, so expand that panel, collapse summary data panel and disable its controls
-              // would be nice to disable collapse/expand options on the panels, but doesn't seem possible at the moment
-              ui.spanel.collapse();
-              ui.rpanel.expand();
-            }
-            ui.alpha.setValue((1 - Number(ui.conf_level.value())/100).toPrecision(1));
-
-
         }
-
-
     };
